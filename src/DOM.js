9dd0052f4a69abffc146549d5b298199b43d8232
VovaParamonov <vovaparamonov2000@mail.ru>
2018-12-03 21:13:03 +0600

    first update

---
+++ src/DOM.js
@@ -0,0 +1,24 @@
+﻿import $ from "jquery";
+
+var $block = $('.block');
+var $counter = $(".counter");
+
+var hp = 100;
+var damage = 10;
+
+$counter.text(hp);
+
+function value_reload(val){
+  hp =val;
+  $('.counter').text(val);
+  $block.attr('data-value', val);
+}
+
+$block.click(function(eventObj){
+  if (hp > 0){
+    let val = hp-damage;
+    value_reload(val);
+  } else {
+    alert("Мертв");
+  }
+});
